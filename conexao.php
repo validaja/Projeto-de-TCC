158ec11f91ad5932d48c4288ce5e46f76b935088
validaja <61952018+validaja@users.noreply.github.com>
2020-04-29 18:04:09 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<<?php 
+define('HOST', '127.0.0.1');
+define('USUARIO','root');
+define('SENHA', 'validaja');
+define('DB','login');
+
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar ao servidor');
+
+?>
